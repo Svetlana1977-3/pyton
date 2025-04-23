10ef6a712a10f15e9c51f24b2137c8706da1cee7
Svetlana1977-3 <Svetlana1977-3>
2025-04-23 21:22:32 +0300

    Update1 my.py

--- my.py
+++ my.py
@@ -1,4 +1,7 @@
 print("Svetlana")
 
-print("вторая строка")
-print("третья строка")
+print("result" "=" "(2**5+10)*3-15)")
+print("result" "=" "(32+10)*5-15)")
+print("result" "=" "42*3-15")
+print("result" "=" "126-15")
+print("result" "=" "111")
