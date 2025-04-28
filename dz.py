ae5737320f19025b2f72d2937c694aa500f54bac
Svetlana1977-3 <Svetlana1977-3>
2025-04-28 18:11:31 +0300

    23.04 my.py

--- dz.py
+++ dz.py
@@ -6,4 +6,16 @@ b=10
 print((a+b)*3-15)
 print("result" "=" "111")
 
+#26.04.2025
+name = "Светлана", "47", "Да"
+print(name)
+
+y = 3
+x = 5
+print(y > x - 2)
+
 #27.04.2025
+myList = ["Светлана", "47", "Да"]
+mySet = set(myList)
+myList = list(mySet)
+print(myList)
